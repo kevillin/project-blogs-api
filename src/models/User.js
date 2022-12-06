1012e10fc5a58df047a5ec60dbf9fdbdb573a46d
Kevillin Santos <kevillin_santos@hotmail.com>
2022-12-06 11:27:41 -0300

    Requisito 10

--- src/models/User.js
+++ src/models/User.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'users',
     underscored: true,
   });
+  
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+         { foreignKey: 'userId', as: 'blog_posts' });
+     };
 
   return User;
 };
